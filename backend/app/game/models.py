efbc035bf1fd8c0dd0bfa43de70dc565fe1e1b43
John Doe <johndoe@example.com>
2024-09-01 08:50:36 +0500

    commmit

--- backend/app/game/models.py
+++ backend/app/game/models.py
@@ -1,16 +1,10 @@
+# game/models.py
 from django.db import models
-from django.contrib.auth.models import User
-
 
 class Game(models.Model):
-    name = models.CharField(max_length=50)
-    genre = models.CharField(max_length=50)
-    price = models.IntegerField()
-    author_of_ad = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
 
-
-    class Meta:
-        verbose_name = 'Game'
-        verbose_name_plural = 'Games'
-        ordering = ['-created_at']
+    def __str__(self):
+        return self.title
