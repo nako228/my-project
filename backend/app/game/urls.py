4851597514f859750d105a02107c2579c33fcbd4
John Doe <johndoe@example.com>
2024-09-02 09:45:49 +0500

    card

--- backend/app/game/urls.py
+++ backend/app/game/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('create-my-game/', views.create_my_game, name='create_my_game'),
     path('game-list/', views.game_list, name='game_list'),
+    path('games/edit/<int:pk>/', views.edit_game, name='edit_game'),  
+    path('games/delete/<int:pk>/', views.delete_game, name='delete_game'),
 ]
--- backend/app/game/views.py
+++ backend/app/game/views.py
@@ -1,19 +1,35 @@
-# game/views.py
-
-from django.shortcuts import render, redirect
-from .forms import GameForm
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Game
+from .forms import GameForm
+
+def game_list(request):
+    games = Game.objects.all()
+    return render(request, 'game_list.html', {'games': games})
 
 def create_my_game(request):
     if request.method == 'POST':
         form = GameForm(request.POST)
         if form.is_valid():
-            form.save()  # Сохраняет новую игру в базе данных
-            return redirect('game_list')  # Перенаправление на страницу списка игр после сохранения
+            form.save()
+            return redirect('game_list')
     else:
         form = GameForm()
     return render(request, 'create_my_game.html', {'form': form})
 
-def game_list(request):
-    games = Game.objects.all()  # Получение всех игр из базы данных
-    return render(request, 'game_list.html', {'games': games})
+def edit_game(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    if request.method == 'POST':
+        form = GameForm(request.POST, instance=game)
+        if form.is_valid():
+            form.save()
+            return redirect('game_list')
+    else:
+        form = GameForm(instance=game)
+    return render(request, 'edit_game.html', {'form': form})
+
+def delete_game(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    if request.method == 'POST':
+        game.delete()
+        return redirect('game_list')
+    return render(request, 'delete_game.html', {'game': game})
